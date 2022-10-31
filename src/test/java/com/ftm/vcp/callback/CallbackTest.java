75193781488ae0da13b486ac2a1b0b8438c73d37
Favio Tejada <favio_tejada@hotmail.com>
2022-10-31 08:32:03 +0100

    Add bean initialization options test

---
+++ src/test/java/com/ftm/vcp/callback/CallbackTest.java
@@ -0,0 +1,38 @@
+package com.ftm.vcp.callback;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.BDDSoftAssertions.thenSoftly;
+
+@ExtendWith(OutputCaptureExtension.class)
+class CallbackTest {
+
+    @Test
+    void should_print_messages_in_correct_order(final CapturedOutput output) {
+        // Given
+        final var context = new AnnotationConfigApplicationContext(CallbackConfig.class);
+        final var bean = context.getBean(ABean.class);
+
+        // When
+        context.close();
+
+        // Then
+        thenSoftly(softly -> {
+            softly.then(bean).isNotNull();
+            softly.then(output.toString()).contains(
+                    "Calling aBean's constructor.",
+                    "Invoking @PostContruct method.",
+                    "Invoking DisposableBean's afterPropertiesSet method.",
+                    "Invoking customInit method.",
+                    "Invoking @PreDestroy method.",
+                    "Invoking DisposableBean's destroy method.",
+                    "Invoking customDestroy method."
+            );
+        });
+    }
+}
