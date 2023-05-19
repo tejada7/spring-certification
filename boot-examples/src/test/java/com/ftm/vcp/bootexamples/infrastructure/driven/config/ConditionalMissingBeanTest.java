4735a3508a89ab92e86478f1334fe7509b2c8fe3
Favio Tejada <favio_tejada@hotmail.com>
2023-05-19 08:14:36 +0200

    Add example @Conditional

---
+++ boot-examples/src/test/java/com/ftm/vcp/bootexamples/infrastructure/driven/config/ConditionalMissingBeanTest.java
@@ -0,0 +1,25 @@
+package com.ftm.vcp.bootexamples.infrastructure.driven.config;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mock.env.MockEnvironment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SpringJUnitConfig(ConditionalBean.class)
+@TestPropertySource(value = "classpath:/application-with-feature-flag-enabled.yaml", properties = "feature-flag=false")
+class ConditionalMissingBeanTest {
+
+    @Test
+    void should_not_initialize_conditional_bean_when_condition_absent(@Autowired ApplicationContext applicationContext) {
+        assertThatThrownBy(() -> applicationContext.getBean(ConditionalBean.class))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
+    }
+}
